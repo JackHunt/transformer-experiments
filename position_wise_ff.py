eea83bb2da76bc6ffec05f62917168b03fe4aeda
Jack Hunt <jackmileshunt@gmail.com>
2024-08-05 23:43:38 +0100

    Create position_wise_ff.py

---
+++ position_wise_ff.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+
+class PositionWiseFF(nn.Module):
+    def __init__(self, d_model, d_embed, dropout_p=0.1):
+        super().__init__()
+
+        self._layers = nn.Sequential(
+            nn.Linear(d_model, d_embed),
+            nn.Linear(d_model, d_embed),
+            nn.Dropout(dropout_p),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self._layers(x)
